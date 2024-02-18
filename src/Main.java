2a1bf7e261d62feb97eb39f053bb8f88c4a6c7d6
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-02-18 15:17:16 +0530

    adding validations

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,36 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        do {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        double anInterRate = scanner.nextDouble();
+            if((principal < 1000) || (principal > 1000000))
+                System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
+        while ((principal < 1000) || (principal > 1000000));
 
-        System.out.print("Period (Years): ");
-        double period = scanner.nextDouble();
+        double anInterRate = -1;
+        do {
+            System.out.print("Annual Interest Rate: ");
+            anInterRate = scanner.nextDouble();
+
+            if((anInterRate <= 0) || (anInterRate > 30))
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+        }
+        while ((anInterRate <= 0) || (anInterRate > 30));
+
+        double period = -1;
+        do {
+            System.out.print("Period (Years): ");
+            period = scanner.nextDouble();
+
+            if((period < 1) || (period > 30))
+                System.out.println("Enter a value between 1 and 30");
+        }
+        while ((period < 1) || (period > 30));
+
 
         double temp = Math.pow(1 + anInterRate/1200,period*12);
         String mortgage = NumberFormat.getCurrencyInstance ().format(principal * (((anInterRate/1200) * (temp))/((temp) - 1)));
