1c9a80781edccab55dbd70a79db3b8921776fc2c
Shammika Wijesinghe <102298368+shux360@users.noreply.github.com>
2024-02-18 10:13:51 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        double anInterRate = scanner.nextDouble();
+
+        System.out.print("Period (Years): ");
+        double period = scanner.nextDouble();
+
+        double temp = Math.pow(1 + anInterRate/1200,period*12);
+        String mortgage = NumberFormat.getCurrencyInstance ().format(principal * (((anInterRate/1200) * (temp))/((temp) - 1)));
+
+
+        System.out.print("Mortgage: "+ mortgage );
     }
 }
